94c28ea18f5862037c6d5ef7a165cc8ab32d657d
Caleb Bennetts <82233759+c675b713@users.noreply.github.com>
2021-11-16 17:22:51 -0600

    minor change

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
 import {loadPieces} from './gameService/loadPieces.gameService.js'
 //import {GameBoard} from '/GameClasses/ChessBoard.js'
 
 addEventListener('DOMContentLoaded', _ => {
     loadPieces.loadPieces()
 })
+
+
+function newG() {
+    this.GameBoard.setUpPieces();
+}
